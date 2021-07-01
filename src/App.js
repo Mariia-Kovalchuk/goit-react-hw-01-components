81a72e1177f0b2f4c12b1e8591dcaea3d352b976
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-07-01 21:58:55 +0300

    components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import FriendList from './components/friend-list/FriendList';
+import friends from './components/friend-list/friends.json';
+import Profile from './components/social-profile/Profile';
+import user from './components/social-profile/user.json';
+import Statistics from './components/statistics/Statistics';
+import statisticalData from './components/statistics/statistical-data.json';
+import TransactionHistry from './components/transaction-history/TransactionHistory';
+import transactions from './components/transaction-history/transactions.json';
+
+const App = () => {
+    return (
+
+    );
+};
+
+export default App;
