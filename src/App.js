0d5bf9fc27bdd073236df9a71cb358e9c66634ff
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-07-02 02:04:43 +0300

    all components render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import FriendList from './components/friend-list/FriendList';
 import friends from './components/friend-list/friends.json';
-import Profile from './components/social-profile/Profile';
-import user from './components/social-profile/user.json';
+import Profile from "./components/social-profile/Profile";
+import user from "./components/social-profile/user.json";
 import Statistics from './components/statistics/Statistics';
 import statisticalData from './components/statistics/statistical-data.json';
 import TransactionHistry from './components/transaction-history/TransactionHistory';
@@ -10,8 +10,21 @@ import transactions from './components/transaction-history/transactions.json';
 
 const App = () => {
     return (
-
+        <div>
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <Statistics title="Upload stats" stats={statisticalData} />
+            <FriendList friends={friends} />
+            <TransactionHistry transactions={transactions} />
+            
+        </div>
     );
 };
+
 
 export default App;
